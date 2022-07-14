cd1355f0d367a7f380d9f9898d69d3f319134f3c
Manuel Casanova <manucasanova@hotmail.com>
2022-07-14 15:29:01 -0700

    Add all functionalities to get all movies

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,27 @@ import NewMovie from './components/NewMovie';
 import MoviesList from './components/MoviesList';
 import GenresList from './components/GenresList';
 
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 function App() {
+
+  const [ movies, setMovies ] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:8001/movies`)
+    .then(function (res) {
+      setMovies([...res.data])
+    })
+  }, [])
+
   return (
     <div className="App">
       <div className="add_movie_and_genre">
       <NewMovie />
       <NewGenre />
       </div>
-      <MoviesList />
+      <MoviesList movies={movies}/>
       <GenresList />
     </div>
   );
